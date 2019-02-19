f7f1ffe0f0baa45fad93836737f3146560e8cffa
Codependency <835770118@qq.com>
2019-02-19 22:32:06 +0800

    播放界面基本功能完成(80%)

--- song.js
+++ song.js
@@ -1,4 +1,5 @@
 $(function(){
+	//从json中获取歌词
 	$.get('lyric.json').then(function(object){
 		let {lyric} = object
 		let array = lyric.split('\n')
@@ -11,4 +12,30 @@ $(function(){
 			console.log(newArr[1])
 		}
 	})
+	let audio = document.createElement('audio')
+	audio.src = "http://dl.stream.qqmusic.qq.com/C400003NktbO34AYJN.m4a?guid=4805131930&vkey=1E9975118C8D16BBEED1C361816A86A2ECE666E7D0FFDC627B8A7F545AFD30F9C62B5ED59FC7175D2DB9AE93F788F584DF502506CD7B94E4&uin=0&fromtag=38"
+    audio.oncanplay = function(){
+    	audio.play()
+		$('.disc-container').addClass('playing')
+    }
+    $('.icon-pause').on('click',function(){
+    	audio.pause()
+    	$('.disc-container').removeClass('playing')
+    })
+    $('.icon-play').on('click',function(){
+    	audio.play()
+    	$('.disc-container').addClass('playing')
+    })
+	//控制播放按钮显示与否
+	$('.disc-container').click(function(){
+		
+	})
+	
+
+		
+		
+		
+		
 })
+
+
