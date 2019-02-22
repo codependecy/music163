c95a2356270656a8207bdba958709338a8469fb6
Codependency <835770118@qq.com>
2019-02-22 21:22:26 +0800

    从json中读取数据完成

--- song.js
+++ song.js
@@ -4,7 +4,19 @@ $(function() {
 	$.get('./song.json').then(function(response) {
 		let songs = response
 		let song = songs.filter(s=>s.id == id)[0]
-		let {url} = song
+		let {url,name,lyric} = song
+		
+		initPlayer.call(undefined,url)
+		initText(name,lyric)
+	})
+	
+	
+	function initText(name,lyric){
+		$('.song-lyric > h1').text(name)
+		parseLyric(lyric)
+	}
+	
+	function initPlayer(url){
 		let audio = document.createElement('audio')
 		audio.src = url
 		audio.oncanplay = function() {
@@ -19,21 +31,42 @@ $(function() {
 			audio.play()
 			$('.disc-container').addClass('playing')
 		})
-	})
-	//从json中获取歌词
-	$.get('lyric.json').then(function(object) {
-		let {
-			lyric
-		} = object
+	}
+	
+	function parseLyric(lyric){
 		let array = lyric.split('\n')
+		let regex = /^\[(.+)\](.*)$/
+		array = array.map(function(string,index){
+			let matches = string.match(regex)
+			if(matches){
+				return {time: matches[1],words:matches[2]}
+			}
+		})
 		let $lyric = $('.scroll-lyric')
-		for(var i = 0; i < array.length; i++) {
-			let newArr = array[i].split(']')
-			let $p = $("<p></p>")
-			$p.text(newArr[1])
+		array.map(function(object){
+			if(!object){return}
+			let $p = $('<p/>')
+			$p.attr('data-time',object.time).text(object.words)
 			$p.appendTo($lyric)
-		}
-	})
+		})
+	}
+	
+	//从json中获取歌词
+//	$.get('lyric.json').then(function(object) {
+//		let {lyric} = object
+//		let array = lyric.split('\n')
+//		let $lyric = $('.scroll-lyric')
+//		for(var i = 0; i < array.length; i++) {
+//			let newArr = array[i].split(']')
+//			let $p = $("<p></p>")
+//			$p.text(newArr[1])
+//			$p.appendTo($lyric)
+//		}
+//	})
+	
+	
+	
+	
 
 	//控制播放按钮显示与否
 	$('.disc-container').click(function() {
